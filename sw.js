dffe40b4af881e6f13047ba9540f068e2c5ed19b
Yutaro ITO <au.yutaro.ito@gmail.com>
2018-10-21 22:28:06 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,106 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v3.6.2/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.2"});
+
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-54fcfaa8d4bef4ad2ba5.js"
+  },
+  {
+    "url": "app.319712d3e7fffa8163a7.css"
+  },
+  {
+    "url": "app-1a244c9f79eec63b2e63.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-59bc0028065f4945c275.js"
+  },
+  {
+    "url": "index.html",
+    "revision": "4260f773e3e1eab5ca6329e53e318b51"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "9e65296fc9d7fb1bfe95e88525e4ca9a"
+  },
+  {
+    "url": "0.bddaef654c66ded1434d.css"
+  },
+  {
+    "url": "component---src-pages-index-js-ede4094a3013e5d7aa5e.js"
+  },
+  {
+    "url": "0-35827a0aed5776eb9bcb.js"
+  },
+  {
+    "url": "static/d/67/path---index-6a9-RvbM5lrwkaWsgorKVIZUwN7edfM.json",
+    "revision": "244051cf135932ef6dc4742f3ccfdcba"
+  },
+  {
+    "url": "component---src-pages-404-js-abe621e2f52f7cdf9f65.js"
+  },
+  {
+    "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "f821c24feca682b4efb3b21dd9f82fdb"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/offline-plugin-app-shell-fallback/index.html", {
+  whitelist: [/^[^?]*([^.?]{5}|\.html)(\?.*)?$/],
+  blacklist: [/\?(.+&)?no-cache=1$/],
+});
+
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https:/, workbox.strategies.networkFirst(), 'GET');
+"use strict";
+
+/* global workbox */
+self.addEventListener("message", function (event) {
+  var api = event.data.api;
+
+  if (api === "gatsby-runtime-cache") {
+    var resources = event.data.resources;
+    var cacheName = workbox.core.cacheNames.runtime;
+    event.waitUntil(caches.open(cacheName).then(function (cache) {
+      return Promise.all(resources.map(function (resource) {
+        return cache.add(resource).catch(function (e) {
+          // ignore TypeErrors - these are usually due to
+          // external resources which don't allow CORS
+          if (!(e instanceof TypeError)) throw e;
+        });
+      }));
+    }));
+  }
+});
